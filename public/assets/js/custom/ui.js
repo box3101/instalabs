649e18c6148987a6234b0af16ae2d8b3e80934a0
box3101 <box4101@nate.com>
2020-11-02 18:00:59 +0900

    작업내용 commit

---
+++ public/assets/js/custom/ui.js
@@ -0,0 +1,24 @@
+// 비쥬얼 슬라이드
+(function () {
+  new Swiper(".main-visual", {
+    slidesPerView: 9,
+    spaceBetween: 23,
+    loop: true,
+    centeredSlides: true,
+    navigation: {
+      nextEl: ".main-slide-next", // 다음 버튼 클래스명
+      prevEl: ".main-slide-prev", // 이번 버튼 클래스명
+    },
+  });
+
+  // 메인 슬라이드
+  new Swiper(".best-slide", {
+    slidesPerView: 4,
+    spaceBetween: 19,
+    loop: true,
+    navigation: {
+      nextEl: ".best-slide-next", // 다음 버튼 클래스명
+      prevEl: ".best-slide-prev", // 이번 버튼 클래스명
+    },
+  });
+})();
